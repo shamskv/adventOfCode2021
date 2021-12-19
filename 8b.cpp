2365eacdccaf47a6e8b44babdba4185fe64fd2dc
Shams Valibhai <shams.valibhai@gmail.com>
2021-12-19 15:29:42 +0000

    challenge 8 (incomplete)

---
+++ 8b.cpp
@@ -0,0 +1,43 @@
+#include <array>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Problem {
+public:
+  vector<string> input;
+  vector<string> output;
+};
+
+int main() {
+  vector<Problem> problems;
+
+  for (string line; getline(cin, line);) {
+    stringstream ss(line);
+    std::string code;
+    problems.emplace_back();
+    while (ss >> code) {
+      if (code == "|")
+        break;
+      problems.back().input.push_back(code);
+    }
+    while (ss >> code) {
+      problems.back().output.push_back(code);
+    }
+  }
+
+  int sum = 0;
+  for (auto &p : problems) {
+    sum = 0;
+    for (auto &c : p.input) {
+      if (c.size() == 2 || c.size() == 3 || c.size() == 4 || c.size() == 7)
+        sum += c.size();
+    }
+    cout << sum << endl;
+  }
+
+  return 0;
+}
