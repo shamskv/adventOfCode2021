e797d16525614d48fee50f7514e95111d635b4b7
Shams Valibhai <shams.valibhai@gmail.com>
2021-12-23 00:11:10 +0000

    challenge 10 part 2 POGGERS

--- 10b.cpp
+++ 10b.cpp
@@ -2,14 +2,130 @@
 #include <map>
 #include <stack>
 #include <string>
-#include <vector>
+
+template <typename T, class Compare> class MyHeap {
+private:
+  T *t;
+  int size, capacity;
+  Compare comparator;
+
+  void increase_capacity() {
+    int old_capacity = capacity;
+    capacity *= 2;
+    T *new_ptr = new T[capacity];
+    std::uninitialized_copy(t, t + old_capacity, new_ptr);
+    delete t;
+    t = new_ptr;
+  }
+
+public:
+  MyHeap() : size(0), capacity(10) { t = new T[capacity]; }
+  ~MyHeap() { delete t; }
+
+  const T &getRoot() { return t[1]; }
+
+  T getAndPopRoot() {
+    if (size < 1)
+      return T();
+    T root = t[1];
+    t[1] = t[size--]; // also "removes" last element
+    for (int pos = 1;;) {
+      bool leftSwap = false, rightSwap = false;
+      int leftChild = pos * 2, rightChild = (pos * 2) + 1;
+      if (leftChild <= size && !comparator(t[pos], t[leftChild])) {
+        leftSwap = true;
+      }
+      if (rightChild <= size && !comparator(t[pos], t[rightChild])) {
+        rightSwap = true;
+      }
+      // Tiebreak if both childs are eligible
+      if (leftSwap && rightSwap) {
+        leftSwap = comparator(t[leftChild], t[rightChild]);
+        rightSwap = !leftSwap;
+      }
+
+      if (leftSwap) {
+        std::swap(t[pos], t[leftChild]);
+        pos = leftChild;
+      } else if (rightSwap) {
+        std::swap(t[pos], t[rightChild]);
+        pos = rightChild;
+      } else {
+        break; // None of the childs was swapped (or they didnt exist)
+      }
+    }
+    return root;
+  }
+
+  void insert(const T &value) {
+    if (size + 1 >= capacity) { // +1 cuz 1-indexed
+      increase_capacity();
+    }
+    t[++size] = value;
+    for (int pos = size; pos > 1; pos /= 2) {
+      if (comparator(t[pos], t[pos / 2])) {
+        std::swap(t[pos], t[pos / 2]);
+      } else {
+        break;
+      }
+    }
+  }
+
+  void print() {
+    for (int i = 1; i <= size; i++)
+      printf("%d ", t[i]);
+    printf("\n");
+  }
+
+  int getSize() { return size; }
+};
+
+template <typename T> class MedianStorage {
+private:
+  MyHeap<T, std::greater<>> left;
+  MyHeap<T, std::less<>> right;
+
+public:
+  void insert(const T &value) {
+    // First insertion by definition
+    if (right.getSize() == 0) {
+      right.insert(value);
+      return;
+    }
+    // Second insertion by definition, check if we need to swap values
+    if (left.getSize() == 0) {
+      if (value > right.getRoot()) {
+        left.insert(right.getAndPopRoot());
+        right.insert(value);
+      } else {
+        left.insert(value);
+      }
+      return;
+    }
+    // Check if belongs to left or right
+    if (value < right.getRoot()) {
+      left.insert(value);
+    } else {
+      right.insert(value);
+    }
+    // Balance if necessary
+    if (left.getSize() > right.getSize()) {
+      right.insert(left.getAndPopRoot());
+    } else if (right.getSize() > left.getSize() + 1) {
+      left.insert(right.getAndPopRoot());
+    }
+  };
+  const T &getMedian() {
+    return right.getRoot();
+  } // Only works for odd # of elements
+};
 
 std::map<char, int> scoreMap = {{'(', 1}, {'[', 2}, {'{', 3}, {'<', 4}};
 std::map<char, char> openMap = {{')', '('}, {']', '['}, {'}', '{'}, {'>', '<'}};
 
 int main() {
   std::string line;
-  std::vector<int> scores;
+  MedianStorage<long> scores;
 
   while (std::cin >> line) {
     std::stack<char> openStack;
@@ -28,19 +144,17 @@ int main() {
       }
     }
     if (valid) {
-      int line_score = 0;
+      long line_score = 0;
       while (!openStack.empty()) {
         line_score *= 5;
         line_score += scoreMap[openStack.top()];
         openStack.pop();
       }
-      scores.push_back(line_score);
+      scores.insert(line_score);
     }
   }
 
-  for (auto &score : scores) {
-    printf("%d\n", score);
-  }
+  printf("%ld\n", scores.getMedian());
 
   return 0;
 }
